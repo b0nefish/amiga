3e0bf0c65465fa0b74070e073159d4d00ae1c925
Sébastien <boulais.sebastien@gmail.com>
2018-06-17 22:11:54 +0200

    update

---
+++ cracking/golden axe/src/fileinfo.s
@@ -0,0 +1,30 @@
+
+info	lea	table(pc),a0
+	lea	end(pc),a1
+	sub.l	a0,a1
+	move.l	a1,d0
+	lsr.l	#3,d0
+	move.w	d0,filecount
+	
+	subq.w	#1,d0
+	moveq	#0,d1
+
+.loop	add.l	4(a0),d1
+	lea	8(a0),a0
+	dbf	d0,.loop
+
+	move.l	d1,size
+	rts
+
+	;----
+
+filecount
+	ds.w	1
+
+size
+	ds.l	1
+
+	;----
+
+table	incbin	'/bin/filetable'
+end	
