01e57688b32c29497d585cc2bfc4c352d32c7a88
Sébastien <boulais.sebastien@gmail.com>
2018-06-17 21:57:50 +0200

    update

--- cracking/golden axe/src/filetable.s
+++ cracking/golden axe/src/filetable.s
@@ -1,54 +1,56 @@
+	; Golden Axe file table
+
 	dc.l	0,0		; $00
-	dc.l	0,0	
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0	
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0	
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0
+	dc.l	0,$1272		; $01	
+	dc.l	0,$2546		; $02
+	dc.l	0,$2d9c		; $03
+	dc.l	0,$17f8		; $04
+	dc.l	0,$1830		; $05
+	dc.l	0,$16b2		; $06
+	dc.l	0,$28d9		; $07
+	dc.l	0,$5641		; $08
+	dc.l	0,$3f27		; $09
+	dc.l	0,$591a		; $0a
+	dc.l	0,$da8		; $0b
+	dc.l	0,$da8		; $0c
+	dc.l	0,$da8		; $0d
+	dc.l	0,$da8		; $0e
+	dc.l	0,$da8		; $0f
 
-	dc.l	0,0		; $10	
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0	
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0	
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0
+	dc.l	0,$da8		; $10	
+	dc.l	0,$63db		; $11 
+	dc.l	0,$5e73		;
+	dc.l	0,$6b49		;
+	dc.l	0,$46f7		;
+	dc.l	0,$462c		;	
+	dc.l	0,$3edc		;
+	dc.l	0,$46ca		;
+	dc.l	0,$2a13		;
+	dc.l	0,$6a40		;
+	dc.l	0,$6e9a		;	
+	dc.l	0,$6b5d		;
+	dc.l	0,$6b13		;
+	dc.l	0,$550f		;
+	dc.l	0,$6b2f		;
+	dc.l	0,$6453		;
 
-	dc.l	0,0		; $20
-	dc.l	0,0
-	dc.l	0,0	
+	dc.l	0,$48b2		; $20
+	dc.l	0,$38be		; $21
+	dc.l	0,$400f		;	
 	dc.l	$5800,$1017e	; $23 (code)
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0
+	dc.l	0,$7541		;
+	dc.l	0,$69e3		;
+	dc.l	0,$6ae7		;
 	dc.l	$15a00,$2d3b	; $27 (data)
-	dc.l	0,0
-	dc.l	0,0	
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0
-	dc.l	0,0	
-	dc.l	0,0
+	dc.l	0,$6949		;
+	dc.l	0,$45f0		;
+	dc.l	0,$34e2		;
+	dc.l	0,$5981		;
+	dc.l	0,$67fc		;
+	dc.l	0,$5f27		;
+	dc.l	0,$5d52		;	
+	dc.l	0,$396a		;
 
-	dc.l	0,0		; $30
+	dc.l	0,$b8e2		; $30
 	dc.l	$18800,$222a	; $31 (data)
-	dc.l	0,0		; $32
+	dc.l	0,$20d4		; $32
