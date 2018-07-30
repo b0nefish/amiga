862001206a2a79b0297fa1cc94cc831a3cbd09f7
Sébastien <boulais.sebastien@gmail.com>
2018-07-30 20:23:31 +0200

    tw cracked

--- cracking/twinworld/src/bootblock.s
+++ cracking/twinworld/src/bootblock.s
@@ -18,11 +18,15 @@ boot	dc.b	'DOS',0
 	dc.l	880
 
 	;----
+	
+	clr.w	$dff180
+
+	;----
 
 	move.w	#2,$1c(a1)
-	move.l	#45000,$24(a1)	; length
-	move.l	#$6520,$28(a1)	; target
-	move.l	#512*2,$2c(a1)	; offset
+	move.l	#8*512*11,$24(a1)	; length
+	move.l	#$50000,$28(a1)		; target
+	move.l	#512*2,$2c(a1)		; offset
 	
 	move.l	4.w,a6
 	jsr	-456(a6)	; doio	
@@ -38,8 +42,22 @@ boot	dc.b	'DOS',0
 	move.w	#$7fff,$dff09c
 
 	;----
+	
+	lea	reloc(pc),a0
+	lea	$40000,a1
+	move.w	#(padding-reloc)-1,d7
+.loop	move.b	(a0)+,(a1)+
+	dbf	d7,.loop
+	jmp	$40000
 
-	jmp	$6520		; bootstrap twinworld
+	;----
+
+reloc	lea	$50000,a0
+	lea	$6520,a1
+	move.w	#(45000/4)-1,d7
+.loop	move.l	(a0)+,(a1)+
+	dbf	d7,.loop
+	jmp	$6520.w		; bootstrap Twinworld
 
 	;----
 
--- cracking/twinworld/src/dosloader.s
+++ cracking/twinworld/src/dosloader.s
@@ -1,20 +1,20 @@
 
 	SECTION	trackloader,CODE_C
 	
-	;OPT	P+
+	OPT	P+
 
-	include	startup.s
+	;include	startup.s
 
 	;----
 	
-	lea	target,a0
-	lea	table(pc),a1
-	move.w	#$29,d0
-	mulu.w	#20,d0
-	movem.l	(a1,d0.l),d0/d1	
-	jsr	loadf(pc)
-	move.l	d1,length
-	rts
+	;lea	target,a0
+	;lea	table(pc),a1
+	;move.w	#$2c,d0
+	;mulu.w	#20,d0
+	;movem.l	(a1,d0.l),d0/d1	
+	;jsr	loadf(pc)
+	;move.l	d1,length
+	;rts
 
 	;----
 
@@ -42,7 +42,7 @@ loadf	bra.w	.load		;
 
 	;----
 
-.load	movem.l	d2-a6,-(sp)	;
+.load	movem.l	d1-a6,-(sp)	;
 
 	;----
 
@@ -80,18 +80,17 @@ loadf	bra.w	.load		;
 	jsr	move(pc)	;
 	jsr	load(pc)	;
 
-	move.l	d1,d6		;
-.copy	lea	decode,a1	;
-	;lea	$4500,a1	;
+.copy	;lea	decode,a1	;
+	lea	$4500,a1	;
 	lea	(a1,d0.w),a1	;
 	move.w	#512*11,d7	;
 	sub.w	d0,d7		;
 	subq.w	#1,d7		;
 .loop	move.b	(a1)+,(a0)+	; copy byte
-	subq.l	#1,d6		; decrease length
+	subq.l	#1,d1		; decrease length
 	dble	d7,.loop	;
 
-	tst.l	d6		; enought data ?
+	tst.l	d1		; enought data ?
 	beq.b	.done		; yep => done
 	
 	jsr	next(pc)	; nop => go next track
@@ -108,7 +107,7 @@ loadf	bra.w	.load		;
 
 	;----
 
-.quit	movem.l	(sp)+,d2-a6	; quit loader
+.quit	movem.l	(sp)+,d1-a6	; quit loader
 	moveq	#1,d0		;
 .rts	rts			;
 
@@ -176,8 +175,8 @@ readtracklen	EQU	((1088*11)/2)+gap
 load	movem.l	d0-a3,-(sp)
 	move.w	#%1000001000010000,$96(a6)
 
-.retry	lea	rawdata,a0
-	;lea	$400,a0
+.retry	;lea	rawdata,a0
+	lea	$400,a0
 	move.l	a0,$20(a6)
 	move.w	#$4000,$24(a6)
 	move.w	#wordsync,$7e(a6)
@@ -197,8 +196,8 @@ load	movem.l	d0-a3,-(sp)
 	
 mask	EQU	$55555555
 
-.decode	lea	decode,a1	;
-	;lea	$4500,a1	;
+.decode	;lea	decode,a1	;
+	lea	$4500,a1	;
 	moveq	#11-1,d6	;
 	move.l	#mask,d0	;
 
@@ -249,13 +248,13 @@ mask	EQU	$55555555
 
 end	;---- datas
 
-length	ds.l	1
+;length	ds.l	1
 
-rawdata	ds.w	readtracklen
-	dc.b	'sebo'
+;rawdata	ds.w	readtracklen
+;	dc.b	'sebo'
 
-decode	ds.b	512*11
-	dc.b	'sebo'
+;decode	ds.b	512*11
+;	dc.b	'sebo'
 
-target	ds.b	$23bc
-uu	dc.b	'sebo'	
+;target	ds.b	$2d16
+;uu	dc.b	'sebo'	
