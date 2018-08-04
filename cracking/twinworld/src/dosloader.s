9f662f6b9fd6516041e04a3066379a4bca4fc0ec
Sébastien <boulais.sebastien@gmail.com>
2018-08-04 10:56:12 +0200

    fix timing

--- cracking/twinworld/src/dosloader.s
+++ cracking/twinworld/src/dosloader.s
@@ -160,8 +160,8 @@ next	btst.b	#2,$100(a5)	;
 	;---- cpu delay
 
 delay	move.l	d7,-(sp)	;
-	move.w	#2561-1,d7	;
-.loop	nop			;
+	move.w	#8000-1,d7	; timing below 8000 could
+.loop	nop			; crash the game
 	dbf	d7,.loop	;
 	move.l	(sp)+,d7	;
 	rts			;
