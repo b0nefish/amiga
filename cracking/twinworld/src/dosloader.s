5cdfbcaf255b0766aadf50eb0c944a8079644687
Sébastien <boulais.sebastien@gmail.com>
2018-10-19 22:36:31 +0200

    update

--- cracking/twinworld/src/dosloader.s
+++ cracking/twinworld/src/dosloader.s
@@ -1,5 +1,5 @@
 
-	SECTION	trackloader,CODE_C
+	;SECTION	trackloader,CODE_C
 	
 	OPT	P+
 
@@ -12,7 +12,7 @@
 	;move.w	#$2c,d0
 	;mulu.w	#20,d0
 	;movem.l	(a1,d0.l),d0/d1	
-	;jsr	loadf(pc)
+	;jsr	load(pc)
 	;move.l	d1,length
 	;rts
 
@@ -33,29 +33,34 @@ table	include	filetable.s	; file look up table
 	; d0> = return code (1 = success, 0 = error)
 	; d1> = length
 
-loadf	bra.w	.load		;
+load	bra.w	.loader		;
 	bra.w	.rts		;
 	bra.w	.rts		;
 	bra.w	.rts		;
 	bra.w	.rts		;
 	bra.w	.rts		;
+
+	;---- 
+
+.loader	movem.l	d1-a6,-(sp)	; backup registers
+
+	move.l	d0,d6		; d6 = disk offset
+	blt.w	.quit		;		
+	move.l	d1,d7		; d7 = file length
+	ble.w	.quit 		;
 
 	;----
 
-.load	movem.l	d1-a6,-(sp)	;
-
-	;----
-
-	lea	$dff000,a6	;
+	lea	$dff000,a6	; custom chip
 	lea	$bfd000,a5	; ciab
 	lea	$bfe001,a4	; ciaa
 
 	;----
 
-	cmpi.l	#(512*11*2*80)-1,d0
-	bgt.w	.quit		;
-	tst.l	d1		;
-	ble.w	.quit		;
+	move.w	#wordsync,$7e(a6)
+	move.w	#%1000001000010000,$96(a6)
+	move.w	#%0111111100000000,$9e(a6)
+	move.w	#%1001010100000000,$9e(a6)
 
 	;----
 
@@ -67,48 +72,154 @@ loadf	bra.w	.load		;
 	bne.b	.ready		;
 
 	;----
-
-	divu.w	#512*11,d0	;
-	move.w	d0,d7		;
-	lsr.w	#1,d7		; 
+			
+	divu.w	#512*11,d6	;
+	move.w	d6,d0		;
+	lsr.w	#1,d0		;
 	bset.b	#2,$100(a5)	; fix head
-	andi.w	#1,d0		; side ?
-	beq.b	.read		;
-	bchg.b	#2,$100(a5)	;
-.read	swap	d0		;
-	jsr	track0(pc)	;
-	jsr	move(pc)	;
-	jsr	load(pc)	;
+	andi.w	#1,d6		; side ?
+	beq.b	.trk0		;
+	bchg.b	#2,$100(a5)	; toggle head
+.trk0	jsr	track0(pc)	;
 
-.copy	;lea	decode,a1	;
-	lea	$4500,a1	;
-	lea	(a1,d0.w),a1	;
-	move.w	#512*11,d7	;
-	sub.w	d0,d7		;
-	subq.w	#1,d7		;
-.loop	move.b	(a1)+,(a0)+	; copy byte
-	subq.l	#1,d1		; decrease length
-	dble	d7,.loop	;
+	;----
 
-	tst.l	d1		; enought data ?
-	beq.b	.done		; yep => done
+	lea	.smc(pc),a1	;
+	swap	d6		;
+	move.w	d6,d1		;
+	neg.w	d1		; self modified code to
+	move.w	d1,2(a1)	; correct target pointer
+
+	;---- move head
+
+	subq.w	#1,d0		;
+	bmi.b	.load		;	
+.loop0	bclr.b	#0,$100(a5)	; step pulse
+	nop			;
+	nop			;
+	nop			;
+	nop			;
+	bset.b	#0,$100(a5)	;
+	bsr.w	delay		; delay
+	dbf	d0,.loop0	;
+
+	;---- load track
+
+wordsync	EQU	$4489
+gap		EQU	350
+readtracklen	EQU	((1088*11)/2)+gap
+
+.load	movem.l	a4/a5,-(sp)	;
+
+	lea	(a0,d6.w),a4	; upper reference
+	lea	(a4,d7.l),a5	; lower reference
+
+.retry	;lea	rawdata(pc),a1	
+	lea	$400,a1
+	move.l	a1,$20(a6)	
+	move.w	#$4000,$24(a6)
+	move.w 	#%0000000000000010,$9c(a6)
+	move.w	#$8000!readtracklen,d0
+	move.w	d0,$24(a6)
+	move.w	d0,$24(a6)
+
+.wait	btst.b	#1,$1f(a6)
+	beq.b	.wait
+
+	move.w	#$4000,$24(a6)
+	move.w 	#%0000000000000010,$9c(a6)
+
+	;---- AmigaDOS Track Decoder
 	
-	jsr	next(pc)	; nop => go next track
-	jsr	load(pc)	; load it
-	moveq	#0,d0		; zero copy offset
-	bra.b	.copy		; goto to copy loop
+mask	EQU	$55555555
 
+.decode	move.l	#mask,d0	;
+	moveq	#11-1,d5	;
+
+.loop1	cmpi.w	#wordsync,(a1)+	; sync
+	bne.b	.loop1		;
+	cmpi.w	#wordsync,(a1)	;
+	beq.b	.loop1		;
+
+	move.l	a1,a3		;
+	move.l	48(a1),d1	; d1 = header checksum 
+	REPT	12		;
+	move.l	(a3)+,d2	;
+	eor.l	d2,d1		;
+	ENDR			;
+	and.l	d0,d1		;
+	bne.w	.retry		; correct header checksum ?
+
+	movem.l	(a1),d1/d2	; decode sector header
+	and.l	d0,d1		; information
+	and.l	d0,d2		;
+	add.l	d1,d1		;
+	or.l	d2,d1		;
+	clr.b	d1		;
+	add.w	d1,d1		; d1 = (block index * 512)
+	lea	(a0,d1.w),a2	; copy target pointer
+	
+	;---- checksum loop
+
+	lea	56(a1),a3	;
+	move.l	52(a1),d1	; d1 = block checksum
+	move.w	#(512/4)-1,d4	; 
+.loop2	move.l	(a3),d2		; odd bits
+	move.l	512(a3),d3	; even bits
+	eor.l	d2,d1		;
+	eor.l	d3,d1		;
+	lea	4(a3),a3	;
+	dbf	d4,.loop2	;
+
+	and.l	d0,d1		; 
+	bne.w	.retry		; not zero ? load again
+
+	;---- 1 byte precision decode loop
+
+	lea	56(a1),a3	; mfm bitmap pointer
+	move.w	#512-1,d4	; block size
+.loop3	cmp.l	a4,a2		; track pointer between
+	blt.b	.out		; file boundary ?
+	cmp.l	a5,a2		; yes => copy
+	bge.b	.out		; no  => next data
+	move.b	(a3),d2		; odd bits
+	move.b	512(a3),d3	; even bits
+	and.b	d0,d2		;
+	and.b	d0,d3		;
+	add.b	d2,d2		;
+	or.b	d3,d2		;
+.smc	move.b	d2,0(a2)	; save decoded byte (smc)
+	subq.l	#1,d7		; decrease file length
+.out	lea	1(a3),a3	; next byte in raw buffer
+	lea	1(a2),a2	; move track pointer
+	dbf	d4,.loop3	; next byte
+	dbf	d5,.loop1	; next block
+
+	;----
+
+	lea	512*11(a0),a0	;
+	clr.w	d6		;
+
+	;----
+
+	movem.l	(sp)+,a4/a5	; restore cia base registers
+
+	tst.l	d7		; more data ?
+	ble.b	.done		; yes => load another track
+	jsr	next(pc)	; no  => load done
+	bra.w	.load		; load next track		
+	
 	;----
 
 .done	bset.b	#3,$100(a5)	; stop drive
 	bset.b	#7,$100(a5)	;
 	bclr.b	#3,$100(a5)	;
 	bset.b	#3,$100(a5)	;
-
+	
 	;----
 
-.quit	movem.l	(sp)+,d1-a6	; quit loader
-	moveq	#1,d0		;
+.quit	movem.l	(sp)+,d1-a6	;
+	moveq	#1,d0		; success
 .rts	rts			;
 
 	;---- go track 0
@@ -129,20 +240,6 @@ track0	btst.b	#4,(a4)		; track 0 ?
 	bsr.b	delay		;
 	rts			;
 
-	;---- move head
-
-move	subq.w	#1,d7
-	bmi.b	.done
-.loop	bclr.b	#0,$100(a5)	; step pulse
-	nop			;
-	nop			;
-	nop			;
-	nop			;
-	bset.b	#0,$100(a5)	;
-	bsr.b	delay		; delay
-	dbf	d7,.loop	;
-.done	rts			;
-
 	;---- next track
 
 next	btst.b	#2,$100(a5)	;
@@ -157,105 +254,20 @@ next	btst.b	#2,$100(a5)	;
 	bsr.b	delay		; delay
 	rts
 	
-	;---- cpu delay
+	;---- timing
 
 delay	move.l	d7,-(sp)	;
-	move.w	#8000-1,d7	; 
-.loop	nop			; 
-	dbf	d7,.loop	;
+	move.w	#8000,d7	; 
+.loop	dbf	d7,.loop	;
 	move.l	(sp)+,d7	;
 	rts			;
 
-	;---- load track
-
-wordsync	EQU	$4489
-gap		EQU	350
-readtracklen	EQU	((1088*11)/2)+gap
-
-load	movem.l	d0-a3,-(sp)
-	move.w	#%1000001000010000,$96(a6)
-
-.retry	;lea	rawdata,a0
-	lea	$400,a0
-	move.l	a0,$20(a6)
-	move.w	#$4000,$24(a6)
-	move.w	#wordsync,$7e(a6)
-	move.w 	#%0000000000000010,$9c(a6)
-	move.w	#%0111111100000000,$9e(a6)
-	move.w	#%1001010100000000,$9e(a6)
-	move.w	#$8000!readtracklen,d0
-	move.w	d0,$24(a6)
-	move.w	d0,$24(a6)
-
-.wait	btst.b	#1,$1f(a6)
-	beq.b	.wait
-
-	move.w	#$4000,$24(a6)
-	move.w 	#%0000000000000010,$9c(a6)
-
-	;---- Amiga Track Decoder
-	
-mask	EQU	$55555555
-
-.decode	;lea	decode,a1	;
-	lea	$4500,a1	;
-	moveq	#11-1,d6	;
-	move.l	#mask,d0	;
-
-.loop1	cmpi.w	#wordsync,(a0)+	; sync
-	bne.b	.loop1		;
-	cmpi.w	#wordsync,(a0)	;
-	beq.b	.loop1		;
-
-	move.l	a0,a2		;
-	move.l	48(a0),d1	; d1 = header checksum 
-	REPT	12		;
-	move.l	(a2)+,d2	;
-	eor.l	d2,d1		;
-	ENDR			;
-	and.l	d0,d1		;
-	bne.w	.retry		; correct header checksum ?
-
-	movem.l	(a0),d1/d2	; decode sector header
-	and.l	d0,d1		; information
-	and.l	d0,d2		;
-	add.l	d1,d1		;
-	or.l	d2,d1		;
-	clr.b	d1		;
-	add.w	d1,d1		; d1 = (sector number * 512)
-	lea	(a1,d1.w),a2	; sector pointer
-
-	lea	56(a0),a3	;
-	lea	512(a3),a4	;
-	move.l	52(a0),d2	; d2 = sector checksum
-	moveq	#(512/4)-1,d5	; 
-
-.loop2	move.l	(a3)+,d3	; odd bits
-	move.l	(a4)+,d4	; even bits
-	eor.l	d3,d2		;
-	eor.l	d4,d2		;
-	and.l	d0,d3		;
-	and.l	d0,d4		;
-	add.l	d3,d3		;
-	or.l	d4,d3		;
-	move.l	d3,(a2)+	; save decoded data
-	dbf	d5,.loop2	;
-	and.l	d0,d2		;
-	bne.w	.retry		; correct sector checksum ?
-	dbf	d6,.loop1	; next sector
-	
-	movem.l	(sp)+,d0-a3	;
-	rts			;
-
-end	;---- datas
+	;---- datas
 
 ;length	ds.l	1
 
 ;rawdata	ds.w	readtracklen
 ;	dc.b	'sebo'
 
-;decode	ds.b	512*11
-;	dc.b	'sebo'
-
 ;target	ds.b	$2d16
-;uu	dc.b	'sebo'	
+end	dc.b	'sebo'	
