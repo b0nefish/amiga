1fab643563073d8c5fb107f206f64b341c89674e
Sébastien <boulais.sebastien@laposte.net>
2019-05-26 11:14:33 +0200

    soh ados file table

---
+++ cracking/superhangon/src/filetable.s
@@ -0,0 +1,212 @@
+offset	SET	0
+
+	;---- SOH data files
+
+	dc.b	'TUN1'
+	dc.l	offset, $1cc8
+
+offset SET offset+$1cc8
+
+	dc.b	'TUN2'
+	dc.l	offset, $15b8	
+
+offset SET offset+$15b8
+
+	dc.b	'TUN3'
+	dc.l	offset, $25f4
+
+offset SET offset+$25f4
+
+	dc.b	'TUN4'
+	dc.l	offset, $1ad4
+
+offset SET offset+$1ad4
+
+	dc.b	'ASIA'
+	dc.l	offset, $1e24
+
+offset SET offset+$1e24
+
+	dc.b	'DSRT'
+	dc.l	offset, $1878
+
+offset SET offset+$1878
+
+	dc.b	'NY1 '
+	dc.l	offset, $1d1c
+
+offset SET offset+$1d1c
+
+	dc.b	'PARS'
+	dc.l	offset, $19ac
+
+offset SET offset+$19ac
+
+	dc.b	'SEAS'
+	dc.l	offset, $1744
+
+offset SET offset+$1744
+
+	dc.b	'MOCL'
+	dc.l	offset, $242e
+
+offset SET offset+$242e
+
+	dc.b	'VOLC'
+	dc.l	offset, $1744
+
+offset SET offset+$1744
+
+	dc.b	'WNDM'
+	dc.l	offset, $1ed4
+
+offset SET offset+$1ed4
+
+	dc.b	'NTHL'
+	dc.l	offset, $1a88
+
+offset SET offset+$1a88
+
+	dc.b	'TOWN'
+	dc.l	offset, $1a04
+
+offset SET offset+$1a04
+
+	dc.b	'VCLM'
+	dc.l	offset, $317e
+
+offset SET offset+$317e
+
+	dc.b	'PHON'
+	dc.l	offset, $24fc
+
+offset SET offset+$24fc
+
+	dc.b 	'ROCK'
+	dc.l	offset, $1e20
+
+offset SET offset+$1e20
+
+	dc.b 	'BUSH'
+	dc.l	offset, $1204
+
+offset SET offset+$1204
+
+	dc.b 	'TRLG'
+	dc.l	offset, $391c
+
+offset SET offset+$391c
+
+	dc.b 	'DEDT'
+	dc.l	offset, $2c12
+
+offset SET offset+$2c12
+
+	dc.b 	'PLM1'
+	dc.l	offset, $4e58
+
+offset SET offset+$4e58
+
+	dc.b 	'PLM2'
+	dc.l	offset, $362a
+
+offset SET offset+$362a
+
+	dc.b 	'CTSM'
+	dc.l	offset, $3d86
+
+offset SET offset+$3d86
+
+	dc.b 	'CTTL'
+	dc.l	offset, $4680
+
+offset SET offset+$4680
+
+	dc.b 	'BARL'
+	dc.l	offset, $ae0
+
+offset SET offset+$ae0
+
+	dc.b 	'GLFC'
+	dc.l	offset, $449c
+
+offset SET offset+$449c
+
+	dc.b 	'SHIB'
+	dc.l	offset, $4582
+
+offset SET offset+$4582
+
+	dc.b 	'CIBI'
+	dc.l	offset, $19da
+
+offset SET offset+$19da
+
+	dc.b 	'KEHN'
+	dc.l	offset, $1e18
+
+offset SET offset+$1e18
+
+	dc.b	'GAMA'
+	dc.l	offset, $25b6
+
+offset SET offset+$25b6
+
+	dc.b 	'TACH'
+	dc.l	offset, $176c
+
+offset SET offset+$176c
+
+	dc.b 	'NGKS'
+	dc.l	offset, $2972
+
+offset SET offset+$2972
+
+	dc.b 	'BDGS'
+	dc.l	offset, $3334
+
+offset SET offset+$3334
+
+	dc.b 	'ELFS'
+	dc.l	offset, $2894
+
+offset SET offset+$2894
+
+	dc.b 	'BRDL'
+	dc.l	offset, $58de
+
+offset SET offset+$58de
+
+	dc.b 	'AFCR'
+	dc.l	offset, $2792
+
+offset SET offset+$2792
+
+	dc.b	'ASCR'
+	dc.l	offset, $2792
+
+offset SET offset+$2792
+
+	dc.b	'AMCR'
+	dc.l	offset, $2792
+
+offset SET offset+$2792
+
+	dc.b	'EUCR'
+	dc.l	offset, $2792
+
+offset SET offset+$2792
+
+	dc.b	'RWAV'
+	dc.l	offset, $c7c
+
+offset SET offset+$c7c
+
+	dc.b	'SWAV'
+	dc.l	offset, $5aa
+
+offset SET offset+$5aa
+
+	dc.b	'SSMK'
+	dc.l	offset, $6e0
+
