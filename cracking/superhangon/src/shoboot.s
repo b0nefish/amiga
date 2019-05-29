a8cfa1bfeb02f5b04ccf43d680bd633db22e1f27
Sébastien <boulais.sebastien@laposte.net>
2019-05-29 22:19:25 +0200

    shoboot

--- cracking/superhangon/src/shoboot.s
+++ cracking/superhangon/src/shoboot.s
@@ -1,1 +1,23 @@
+offset	SET	0
+
 	dc.b	'SHO.boot'
+
+	;---- SOH data files
+
+	dc.b	'SLOG'
+	dc.l	offset, $52*512
+
+offset SET offset+($52*512)
+
+	dc.b	'MUSC'
+	dc.l	offset, $67*512	
+
+offset SET offset+($67*512)
+
+	dc.b	'LDSC'
+	dc.l	offset, $40*512
+
+offset SET offset+($40*512)
+
+	dc.b	'GAME'
+	dc.l	offset, $233*512
