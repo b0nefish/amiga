b67f0edb99b05e00e02e6821540753176c24fbc9
Sébastien <boulais.sebastien@laposte.net>
2019-05-30 10:14:15 +0200

    update

--- cracking/superhangon/src/trackloader.s
+++ cracking/superhangon/src/trackloader.s
@@ -8,30 +8,16 @@
 	
 	include	/src/startup.s
 
-	;---- capture index
+	;---- 
 
-rip1	lea	target(pc),a0
-	lea	index1(pc),a1
-	jsr	loadfile(pc)
-	rts
-
-	;---- capture files
-
-rip2	lea	target(pc),a0	
-	lea	index2+8+(0*8)(pc),a1
-	moveq	#0,d2
-	move.w	#20-1,d7	; nb files to load
-
-.loop	jsr	loadfile(pc)	;
-	lea	(a0,d1.w),a0	;
-	lea	8(a1),a1	;
-	andi.l	#$ffff,d1	;
-	add.l	d1,d2		;
+	lea	target(pc),a0	;
+	lea	shogfx(pc),a1	;
+	move.w	#0,d0		;
+	jsr	loadfile(pc)	;
 	move.l	d0,fname	;
 	move.w	d1,fsize	;
-	dbf	d7,.loop	;
-
 	move.l	d2,loadlen	;
+
 	rts
 
 	;----
@@ -40,7 +26,8 @@ rip2	lea	target(pc),a0
 	; Trackloader
 	;
 	; >a0 = target
-	; >a1 = file pointer
+	; >a1 = index
+	; >d0 = file id
 	; d0> = file name 
 	; d1> = file size
 	
@@ -54,25 +41,39 @@ loadfile
 	move.b	#%11111111,$100(a5)
 	bclr.b	#7,$100(a5)	; motor on
 	bclr.b	#3,$100(a5)	; df0
-	bclr.b	#2,$100(a5)	; set side
 
-	
 	btst.b	#2,(a4)		; check disk inserted
-	beq.b	.quit		;
+	beq.w	.done		;
 .wait	btst.b	#5,(a4)		; wait disk ready
 	bne.b	.wait		;
 
 	;----
 
+	move.w	(a1)+,d1	; get side
+	beq.b	.a		;
+	bchg.b	#2,$100(a5)	; change side
+
+	;----
+
+.a	lsl.w	#3,d0		;
+	lea	8(a1,d0.w),a1	;
+	
 	moveq	#0,d0		;
 	moveq	#0,d1		;
+	moveq	#0,d2		;
 	move.b	4(a1),d0	; start track
 	move.b	5(a1),d1	; block
 	move.w	6(a1),d2	; file size
 
 	;----
 
-	jsr	track0(pc)	; go track 0
+	btst.b	#2,$100(a5)	; convert shoboot file
+	beq.b	.b		; size in byte
+	mulu.w	#512,d2		;
+
+	;----
+
+.b	jsr	track0(pc)	; go track 0
 
 	move.w	d0,d7		;
 	jsr	move(pc)	;
@@ -93,10 +94,10 @@ loadfile
 .load	jsr	load(pc)	; load track
 
 .loop	move.b	(a2)+,(a0)+	;
-	subq.w	#1,d2		;
+	subq.l	#1,d2		;
 	dble	d7,.loop	;	
 	
-	tst.w	d2		; enought data ?
+	tst.l	d2		; enought data ?
 	beq.b	.done		; yes => leave
 	jsr	next(pc)	; no  => next track
 
@@ -230,14 +231,29 @@ mask	EQU	$5555
 .wblt2	btst.b	#6,2(a6)
 	bne.b	.wblt2
 	
+	tst.w	(a2)		; when decoded first two bytes
+	bne.w	.retry		; must be zero
+
+	;---- decrypt
+
+	btst.b	#2,$100(a5)	; decrypt shoboot data
+	beq.b	.chksum		; only (side 0)		
+
+	lea	buffer+4(pc),a0	;	
+	move.w	#(5640/4)-2,d7	;
+	move.l	#$12345678,d0	;
+.loop2	eor.l	d0,(a0)		; runtime xor decryption
+	move.l	(a0)+,d0	;
+	dbf	d7,.loop2	;
+	
 	;---- checksum
 	
 .chksum	lea	buffer+2(pc),a0	;
 	moveq	#0,d0		;
 	move.w	(a0)+,d0	; get track number 
-	move.w	#(5640/4)-2-1,d7
-.loop2	add.l	(a0)+,d0	; sum
-	dbf	d7,.loop2	;
+	move.w	#(5640/4)-3,d7	;
+.loop3	add.l	(a0)+,d0	; sum
+	dbf	d7,.loop3	;
 
 	cmp.l	(a0),d0		; compare checksum
 	bne.w	.retry		; retry if different
@@ -255,11 +271,23 @@ loadlen	ds.l	1
 
 	;---- table
 
-index1	dc.l	'INDX'
+index0	dc.l	'IDX0'	; index for shoboot files
+	dc.b	2,0
+	dc.w	512
+
+index1	dc.l	'IDX1'	; index for shografx files
 	dc.b	1,0
 	dc.w	512
 
-index2	incbin	/bin/shografx/table
+shoboot	dc.w	0	; side 0
+	; files length are in block (multiple of 512 bytes)
+	; and are crypted on disk with xor runtime algorithm
+	incbin	/bin/shoboot/table
+
+shogfx	dc.w	1	; side 1
+	; files length are in bytes
+	; no encryption
+	incbin	/bin/shografx/table
 
 	;---- buffers
 	
@@ -270,6 +298,9 @@ diskdata
 buffer	ds.w	705*4
 	dc.b	'tail'
 
-target	ds.b	160398
+target	ds.b	$1cc8
+	;ds.b	$233*512
+	;ds.b	160398
 	;ds.b	239616
+
 end	dc.b	'tail'	
